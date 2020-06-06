a48825ede7fe8f0a5ceefe36438dbfde7ed10600
imtiaz051 <imtiazalisoomro@gmail.com>
2020-06-06 22:45:47 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/deployment of react app</code> on surge
         </p>
         <a
           className="App-link"
